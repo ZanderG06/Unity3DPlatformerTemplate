0ba044cd9e26b9a5880bce9f31d846ec65f5a8ae
Zander Goodwin <zanderngoodwin@gmail.com>
2026-02-23 17:01:55 -0400

    Continued working on the PlayerSwapManager

--- Assets/PlayerSwapManager.cs
+++ Assets/PlayerSwapManager.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 
 public class PlayerSwapManager : MonoBehaviour
 {
-    public static int currentPlayerIndex { get; private set; }
+    public static int activePlayerIndex { get; private set; }
 
     public static PlayerController ActivePlayer
     {
         get
         {
             if (PlayerController.players.Count == 0) return null;
-            return PlayerController.players[currentPlayerIndex];
+            return PlayerController.players[activePlayerIndex];
         }
+    }
+
+    public static void SwapToNextPlayer()
+    {
+        if (PlayerController.players.Count <= 1) return;
+
+        //Disables the current active player
+        PlayerController.players[activePlayerIndex].enabled = false;
+
+        activePlayerIndex++;
+        if(activePlayerIndex >= PlayerController.players.Count) activePlayerIndex = 0;
+
     }
 }
